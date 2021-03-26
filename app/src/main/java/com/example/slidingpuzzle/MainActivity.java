5c69a7c997b75ef16b823e1c75b4756f276ab333
PSYcode04 <psy.code04@gmail.com>
2021-03-26 22:55:13 +0900

    Add the scale select buttons

--- app/src/main/java/com/example/slidingpuzzle/MainActivity.java
+++ app/src/main/java/com/example/slidingpuzzle/MainActivity.java
@@ -3,12 +3,19 @@ package com.example.slidingpuzzle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn1 = (Button) findViewById(R.id.button1);
+        Button btn2 = (Button) findViewById(R.id.button2);
+        // Shuffle 버튼 추가
     }
+
 }
